876c688b0656a986766b89122f68cd4395abfc38
dieall <aldiharun07@gmail.com>
2025-05-14 23:29:57 +0700

    Fix Vercel deployment configuration

--- api/index.php
+++ api/index.php
@@ -1,4 +1,20 @@
 <?php
 
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php'; 
+// Set the base path for the Laravel application.
+$basePath = realpath(__DIR__ . '/..');
+
+// Load the Laravel application
+require $basePath . '/vendor/autoload.php';
+
+$app = require_once $basePath . '/bootstrap/app.php';
+
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response); 
